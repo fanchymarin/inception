85e5bb7b4410030c813664dc821f8b2ae48c6571
Fran Marín Pérez <francisco.marinperez.practicas@telefonica.com>
2024-03-31 18:13:48 +0200

    redis working

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -79,11 +79,12 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_LOG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
-
-
+define( 'WP_REDIS_PATH', '/var/run/redis/redis.sock' );
+define( 'WP_REDIS_SCHEME', 'unix' );
 
 /* That's all, stop editing! Happy publishing. */
 
