9e613fdb19ceb3692a1f24d4d7316b58286d8279
fmarin-p <fmarin-p@student.42madrid.com>
2024-05-24 11:30:34 +0200

    wp cp line removed

--- srcs/requirements/wordpress/conf/wp-init.sh
+++ srcs/requirements/wordpress/conf/wp-init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cp /wp-config.php .
-cp /resume.html .
 wp core install --url=$DOMAIN_NAME/ --title=$WP_NAME \
     --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD \
     --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
